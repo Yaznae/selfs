a07e34abb171b2f1cfb371a18b1ae2d645323110
Yaznae <kiro.tdm@gmail.com>
2024-03-15 17:08:05 +0200

    add image.js

---
+++ commands/image.js
@@ -0,0 +1,21 @@
+const gis = require('async-g-i-s');
+
+module.exports = {
+    name: 'copy',
+    async execute(msg, args) {
+        await msg.delete();
+        if (!args.length) return;
+        let keywords = args.join(' ');
+
+        results = await gis(keywords);
+
+        if (!results.length) {
+            return msg.channel.send(`no **results** found .`).then(message => {
+                setTimeout(() => message.delete(), 5_000);
+            }).catch(err => console.error(err));
+        } else {
+            return msg.channel.send(results[0].url);
+        };
+
+    }
+}
